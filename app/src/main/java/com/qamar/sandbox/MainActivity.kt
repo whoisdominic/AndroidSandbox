f55d96b4a68a86b93f5881ce719e53dcfc3eca61
Dominic Cobb <dominicshajaa@icloud.com>
2020-10-06 20:41:36 -0700

    fragments example

--- app/src/main/java/com/qamar/sandbox/MainActivity.kt
+++ app/src/main/java/com/qamar/sandbox/MainActivity.kt
@@ -2,10 +2,34 @@ package com.qamar.sandbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)
+            commit()
+        }
+
+        btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/qamar/sandbox/SecondFragment.kt
@@ -0,0 +1,14 @@
+package com.qamar.sandbox
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+
+
+}
